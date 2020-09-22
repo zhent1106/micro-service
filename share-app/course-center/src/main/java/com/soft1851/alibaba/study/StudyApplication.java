1dae09a91b00dbb1aa5efd5c17d0ead1ef9882bc
zhent1106 <1299088269@qq.com>
2020-09-22 15:50:10 +0800

    spring-cloud-alibaba起步

---
+++ share-app/course-center/src/main/java/com/soft1851/alibaba/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package com.soft1851.alibaba.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
---
+++ share-app/course-center/src/test/java/com/soft1851/alibaba/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft1851.alibaba.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
